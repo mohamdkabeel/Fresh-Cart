6bf94ec1533c367fbb78fac4074a1385faa1a947
mohamdkabeel <kabeelmohamed22@gmail.com>
2026-04-26 01:10:17 +0300

    update after publish Api Link

--- src/component/CatWidgt/ChatWidget.jsx
+++ src/component/CatWidgt/ChatWidget.jsx
@@ -13,7 +13,7 @@ export default function ChatWidget() {
         setMessages([...messages, { role: "user", text: userMsg }]);
         setInput("");
 
-        const res = await fetch("http://127.0.0.1:8000/chat", {
+        const res = await fetch("https://chatbotai-wypy.onrender.com", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
