5c69c2a66acbf5f9eedbc81b3a57aa4e73e23f0e
mohamdkabeel <kabeelmohamed22@gmail.com>
2026-04-26 16:47:41 +0300

    p

--- src/component/CatWidgt/ChatWidget.jsx
+++ src/component/CatWidgt/ChatWidget.jsx
@@ -16,7 +16,7 @@ export default function ChatWidget() {
         setLoading(true);
 
         try {
-            const res = await fetch("https://chatbotai-production-83bf.up.railway.app/chat", {
+            const res = await fetch("https://chatbotai-wypy.onrender.com/chat", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({
